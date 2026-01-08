208f8beb5d9cbc9853a23e53d5057e71a4741d5c
Kasmalr32 <kasmalrasiswardhana@gmail.com>
2026-01-08 19:50:52 +0700

    backend login/register, role logic : user(wip), dokter, admin, superadmin

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/apiRoutes');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middleware Global
+// 🔥 Middleware Global (INI WAJIB)
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // ⬅️ PENTING
+app.use(express.urlencoded({ extended: true }));
 
 // Routing
 app.use('/api', apiRoutes);
 
 // Error Handling Dasar
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.status(404).json({ message: 'Endpoint tidak ditemukan' });
 });
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}`);
-    console.log(`Koneksi DB Host: ${process.env.DB_HOST}`);
 });

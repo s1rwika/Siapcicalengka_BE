64eb2d022bdcc5fd3ddf8b19294016c9c4522100
s1rwika <sirwika11@gmail.com>
2026-01-23 16:19:16 +0700

    PENAMBAHAN REVIEW, RIWAYAT PASIEN DAN STATISTIK PENYAKIT 2

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Zeagtluke123',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'siap_cicalengka'
 });
 
